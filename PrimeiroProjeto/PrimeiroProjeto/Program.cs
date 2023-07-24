b5fc75707acf09ca7cb15e9811c656d1b7916e08
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2023-07-24 07:28:18 -0300

    c# - 01

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
